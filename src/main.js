6b318087057c40b167b430582c2a749831969589
Grzegorz on Mac <grzegorz.zajac@me.com>
2021-07-20 02:06:26 +0200

    add amplify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import Amplify, * as AmplifyModules from 'aws-amplify';
-import { AmplifyPlugin } from 'aws-amplify-vue';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
-Vue.use(AmplifyPlugin, AmplifyModules);
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 createApp(App).mount('#app')
